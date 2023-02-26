2ced11793010ae5af3f953597aeabcb25466db01
Lahcen001 <lahcen0000lh@gmail.com>
2023-02-26 10:03:41 -0600

    add complex logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,18 +9,45 @@ export class AppComponent {
   title = 'team-app';
   newMemberName='';
   members: string[] =[];
+  errorMessage="";
+  numberOfTeams:number | "" = ""  ;
 
   onInput(member:string){ 
     this.newMemberName = member
   }
-
-
   addMember(){
+
+    if(!this.newMemberName){
+      this.errorMessage = "Name can't be empty"
+      return;
+    }
+
     this.members.push(this.newMemberName)
-    console.log(this.members)
+    this.newMemberName = ""
+   
   }
+  NumberofTeamInput(value:string){
 
+this.numberOfTeams = Number(value)
+}
+
+generateTeams(){
+
+if(!this.numberOfTeams || this.numberOfTeams<=0 ){
+  return
+}
+
+
+const allMembers =[...this.members]
+
+for(let i=0; i<this.numberOfTeams; i++){
+
+const randomIndex = Math.floor(Math.random()*allMembers.length)
+
+console.log("first",randomIndex)
+ 
+}
+
+}
   
-
-
 }
