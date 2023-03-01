f72f4cee0b104adbb56135100c7b14cc7adc2d23
Lahcen001 <lahcen0000lh@gmail.com>
2023-03-01 00:09:59 -0600

    add team group

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ export class AppComponent {
   members: string[] =[];
   errorMessage="";
   numberOfTeams:number | "" = ""  ;
+  teams: string[][] =[]
 
   onInput(member:string){ 
     this.newMemberName = member
@@ -34,19 +35,43 @@ this.numberOfTeams = Number(value)
 generateTeams(){
 
 if(!this.numberOfTeams || this.numberOfTeams<=0 ){
-  return
+  this.errorMessage = "Invalid number of Teams"
+  return;
 }
 
+if(this.members.length < this.numberOfTeams){
+  this.errorMessage = "Not enough members";
+  return;
+}
+
+this.errorMessage = "";
 
 const allMembers =[...this.members]
+
+while(allMembers.length){
 
 for(let i=0; i<this.numberOfTeams; i++){
 
 const randomIndex = Math.floor(Math.random()*allMembers.length)
 
-console.log("first",randomIndex)
- 
+const member = allMembers.splice(randomIndex,1)[0];
+
+if(this.teams[i]){
+
+  this.teams[i].push(member)
 }
+else {
+  this.teams[i]=[member]
+}}
+
+}
+
+
+
+this.members =[];
+this.numberOfTeams ='';
+
+console.log("teams : ", this.teams)
 
 }
   
